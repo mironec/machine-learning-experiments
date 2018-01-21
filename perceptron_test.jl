0e8f3a9b880007a7831541495ba8305729320875
Miron Zelina <mironzelinajr@gmail.com>
2018-01-21 13:54:09 +0000

    Add Windrow-Hoff delta rule to train the last layer
    There seems to be an issue with training a XOR gate, which should be
    impossible, but should also settle on OR, but it diverges.

--- perceptron_test.jl
+++ perceptron_test.jl
@@ -16,10 +16,10 @@ function stepF(x::AbstractFloat)::AbstractFloat
     if x >= 0
         return 1
     end
-    return -1
+    return 0
 end
 
-function epoch(patterns, neurons)::AbstractFloat
+function epoch(patterns, neurons, η)::AbstractFloat
     numLayers = length(neurons)
     numPatterns = length(patterns)
     errorAcc = 0.0
@@ -32,8 +32,9 @@ function epoch(patterns, neurons)::AbstractFloat
         for l = 2:numLayers
             for i = 1:length(neurons[l])
                 neurons[l][i].valueOut = 0
+                neurons[l][i].valueOut += neurons[l][i].weightsIn[1]
                 for j = 1:length(neurons[l-1])
-                    neurons[l][i].valueOut += neurons[l-1][j].valueOut * neurons[l][i].weightsIn[j]
+                    neurons[l][i].valueOut += neurons[l-1][j].valueOut * neurons[l][i].weightsIn[j+1]
                 end
                 neurons[l][i].valueOut = stepF(neurons[l][i].valueOut)
             end
@@ -43,6 +44,10 @@ function epoch(patterns, neurons)::AbstractFloat
         for i = 1:length(neurons[numLayers])
             errorAcc2 += (patterns[p].outputValues[i] - neurons[numLayers][i].valueOut) ^ 2.0
             print("Expected $(patterns[p].outputValues[i]), got $(neurons[numLayers][i].valueOut).\n")
+            neurons[numLayers][i].weightsIn[1] += η * (patterns[p].outputValues[i] - neurons[numLayers][i].valueOut) #bias weight
+            for j = 2:length(neurons[numLayers][i].weightsIn)
+                neurons[numLayers][i].weightsIn[j] += η * (patterns[p].outputValues[i] - neurons[numLayers][i].valueOut) * neurons[numLayers-1][j-1].valueOut
+            end
         end
         errorAcc2 = sqrt(errorAcc2 / length(neurons[numLayers]))
         errorAcc += errorAcc2
@@ -52,18 +57,21 @@ function epoch(patterns, neurons)::AbstractFloat
 end
 
 function createNeuron(numPreviousLayers)
-    return Perceptron([rand() for i=1:numPreviousLayers], 0)
+    return Perceptron([rand()-0.5 for i=1:numPreviousLayers+1], 0)
 end
 
-function train(patterns, layerNums = [2,1], numEpochs = 1)
+function train(patterns, layerNums = [2,1], numEpochs = 100, η = 0.05)
     currentEpoch = 1
     neurons = Array{Array{Perceptron}}(length(layerNums))
     for i = 2:length(layerNums)
         neurons[i] = [createNeuron(layerNums[i-1]) for j=1:layerNums[i]]
     end
     while(currentEpoch <= numEpochs)
-        error = epoch(patterns, neurons)
+        error = epoch(patterns, neurons, η)
         print("Epoch $currentEpoch: error $error\n")
+        if(error == 0.0)
+            break
+        end
         currentEpoch += 1
     end
 end
